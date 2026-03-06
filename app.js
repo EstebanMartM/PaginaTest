d2e82635bdb5800899e47a3ce8b4573c911bfcbf
Iván Guijarro <iguijarrolillo@gmail.com>
2026-03-06 19:20:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -30,6 +30,7 @@
   const LIB_SOURCES = [
     { id: 'Redes', label: 'Redes', url: './baterias/Redes.txt' },
     { id: 'TAR', label: 'TAR', url: './baterias/TAR.txt' },
+    { id: 'ADA', label: 'ADA', url: './baterias/Ada.txt' },
   ];
 
   const EXAM_SOURCE_ID = 'Redes';
